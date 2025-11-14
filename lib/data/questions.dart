3c24ef59a932c24dcbdb43f67b12942ded9e803b
Abhinav Priyadarshi <rush2rain@gmail.com>
2025-11-14 18:03:38 +0530

    8th question

--- lib/data/questions.dart
+++ lib/data/questions.dart
@@ -10,11 +10,10 @@ class ReportQuestion {
 const List<ReportQuestion> dailyReportQuestions = [
   ReportQuestion(id: 1, text: 'Application for registration'),
   ReportQuestion(id: 2, text: 'Application for amendment'),
-  ReportQuestion(id: 3, text: 'Cancellation proceedings for registration'),
+  ReportQuestion(id: 3, text: 'Cancellation proceedings for registration by taxpayer'),
   ReportQuestion(id: 4, text: 'Application for registration as TDS/TCS'),
   ReportQuestion(id: 5, text: 'Application for registration as Non Resident Taxable Person'),
   ReportQuestion(id: 6, text: 'Suo-Moto cancellation proceedings'),
   ReportQuestion(id: 7, text: 'Revocation of cancelled registration'),
-  // ToDo: Add the 8th question when available
-  // ReportQuestion(id: 8, text: 'Placeholder for 8th question'),
+  ReportQuestion(id: 8, text: 'Pending report verification'),
 ];
